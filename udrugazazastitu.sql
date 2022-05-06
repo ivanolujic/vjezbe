42f46711ca72224456c602814b673a512284f183
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-06 19:18:40 +0200

    Create udrugazazastitu.sql

---
+++ udrugazazastitu.sql
@@ -0,0 +1,25 @@
+# tablice za udrugu za zastitu zivotinja
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\vjezbe\udrugazazastitu.sql
+
+drop database if exists udrugazazastitu;
+create database udrugazazastitu;
+use udrugazazastitu;
+
+create table djelatnik(
+    ime varchar(50),
+    prezime varchar(50),
+    oib varchar(50),
+    email varchar(50)
+);
+
+create table sticenik(
+    ime varchar(50),
+    vrsta varchar(50),
+    starost varchar(50)
+);
+
+create table prostor(
+    boks varchar(50),
+    nastamba varchar(50),
+    krletka varchar(50)
+);
