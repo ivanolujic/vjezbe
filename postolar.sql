5aaae867e99d83cae22f079fd1ba5fdb8d64c687
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-08 19:03:29 +0200

    Create postolar.sql

---
+++ postolar.sql
@@ -0,0 +1,43 @@
+# Postolar popravlja obuću. Jedan korisnik može postolaru donijeti više komada obuće na popravak dok jedan komad obuće može biti više puta na popravku.
+# Postolar ima šegrta koji sudjeluje u određenim popravcima obuće
+
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\vjezbe\postolar.sql
+
+drop database if exists postolar;
+create database postolar;
+use postolar;
+
+create table obuca(
+    vrsta varchar(50),
+    velicina varchar(50),
+    boja varchar(50),
+    proizvodjac varchar(50)
+);
+
+create table djelatnik(
+    osoba varchar(50),
+    datumrodjenja varchar(50),
+    iban varchar(50),
+    statusdjelatnika varchar(50)
+);
+
+create table osoba(
+    ime varchar(50),
+    prezime varchar(50),
+    oib varchar(50),
+    email varchar(50),
+    adresa varchar(50)
+);
+
+create table korisnik(
+    osoba varchar(50),
+    nacinplacanja varchar(50)
+);
+
+create table usluga(
+    obuca varchar(50),
+    djelatnik varchar(50),
+    korisnik varchar(50),
+    cijena varchar(50),
+    nacinplacanja varchar(50)
+);
