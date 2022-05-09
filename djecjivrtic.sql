fcd3a0d39b841f54bcf776c246ff7781c0f971f8
Ivan Olujic <104625943+ivanolujic@users.noreply.github.com>
2022-05-09 20:06:54 +0200

    Update djecjivrtic.sql

--- djecjivrtic.sql
+++ djecjivrtic.sql
@@ -6,6 +6,7 @@ create database djecjivrtic;
 use djecjivrtic;
 
 create table osoba(
+    sifra int not null primary key auto_increment
     ime varchar(50),
     prezime varchar(50),
     oib varchar(50),
@@ -13,17 +14,20 @@ create table osoba(
 );
 
 create table odgojiteljica(
+    sifra int not null primary key auto_increment
     iban varchar(50),
     email varchar(50),
     osoba varchar(50)
 );
 
 create table dijete(
+    sifra int not null primary key auto_increment
     osoba varchar(50),
     roditelj varchar(50)
 );
 
 create table skupina(
+    sifra int not null primary key auto_increment
     naziv varchar(50),
     odgojiteljica varchar(50),
     maximumdjece varchar(50)
