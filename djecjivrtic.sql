91ca0f492359c97685cc0233d141c908e3fb8e22
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-10 17:52:11 +0200

    Odabir tipa podatka

--- djecjivrtic.sql
+++ djecjivrtic.sql
@@ -1,34 +1,35 @@
 # djecji vrtic
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\vjezbe\djecjivrtic.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\vjezbe\djecjivrtic.sql
 
 drop database if exists djecjivrtic;
 create database djecjivrtic;
 use djecjivrtic;
 
 create table osoba(
-    sifra int not null primary key auto_increment
-    ime varchar(50),
-    prezime varchar(50),
-    oib varchar(50),
-    godiste varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11) not null,
+    datumrodjenja datetime not null
 );
 
 create table odgojiteljica(
-    sifra int not null primary key auto_increment
-    iban varchar(50),
-    email varchar(50),
-    osoba varchar(50)
+    sifra int not null primary key auto_increment,
+    iban varchar(50) not null,
+    email varchar(100),
+    osoba int not null
 );
 
 create table dijete(
-    sifra int not null primary key auto_increment
-    osoba varchar(50),
-    roditelj varchar(50)
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    boravak boolean not null,
+    cijena decimal(18,2)
 );
 
 create table skupina(
-    sifra int not null primary key auto_increment
-    naziv varchar(50),
-    odgojiteljica varchar(50),
-    maximumdjece varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    odgojiteljica int not null,
+    maximumdjece int not null
 );
