57a5f66630c632d2f8bb2f8e38009399ff53f333
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-10 21:41:30 +0200

    Update frizerskisalon.sql
    dodani tipovi podataka i alter table

--- frizerskisalon.sql
+++ frizerskisalon.sql
@@ -1,31 +1,41 @@
-# tablice za frizerski salon
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\vjezbe\frizerskisalon.sql
+# U frizerskom salonu radi više djelatnica. Jedna djelatnica na dan radi s više korisnika. Korisnik tijekom jednog posjeta koristi jednu usluga
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\vjezbe\frizerskisalon.sql
 
 drop database if exists frizerskisalon;
 create database frizerskisalon;
 use frizerskisalon;
 
 create table djelatnica(
-    oib varchar(50),
-    iban varchar(50),
-    osoba varchar(50)
+    sifra int not null primary key auto_increment,
+    oib char(11) not null,
+    iban varchar(50) not null,
+    osoba int not null
 );
 
 create table usluga(
-    sisanje varchar(50),
-    brijanje varchar(50),
-    pranjekose varchar(50),
-    minival varchar(50),
-    farbanje varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    cijena decimal(18,2) not null,
+    djelatnica int not null,
+    klijent int not null
 );
 
 create table klijent(
-    osoba varchar(50),
-    email varchar(50),
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    email varchar(100),
     telefon varchar(50)
 );
 
 create table osoba(
-    ime varchar(50),
-    prezime varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null
 );
+
+alter table djelatnica add foreign key (osoba) references osoba(sifra);
+alter table klijent add foreign key (osoba) references osoba(sifra);
+
+alter table usluga add foreign key (djelatnica) references djelatnica(sifra);
+alter table usluga add foreign key (klijent) references klijent(sifra);
+
