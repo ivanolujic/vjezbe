44ccc2798d4744bd3192dd19b9aa4398565059f6
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-10 22:00:23 +0200

    Update muzej.sql
    određivanje tipa podataka

--- muzej.sql
+++ muzej.sql
@@ -1,38 +1,41 @@
 # U muzeju postoji više izložaba. Jedna izložba ima više djela. Svaki kustos je zadužen za jednu izložbu. Svaka izložba ima jednog sponzora.
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\vjezbe\muzej.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\vjezbe\muzej.sql
 
 drop database if exists muzej;
 create database muzej;
 use muzej;
 
 create table izlozba(
-    naziv varchar(50),
-    autor varchar(50),
-    pocetak varchar(50),
-    zavrsetak varchar(50),
-    cijena varchar(50),
-    kustos varchar(50),
-    sponzor varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    pocetak datetime not null,
+    zavrsetak datetime,
+    cijena decimal(18,2),
+    kustos int not null,
+    sponzor int not null
 );
 
 create table djelo(
-    nazivdjela varchar(50),
-    autor varchar(50),
-    izlozba varchar(50),
-    kustos varchar(50)
+    sifra int not null primary key auto_increment,
+    nazivdjela varchar(100) not null,
+    autor varchar(50) not null,
+    izlozba int not null,
+    kustos int not null
 );
 
 create table kustos(
-    ime varchar(50),
-    prezime varchar(50),
-    izlozba varchar(50),
-    oib varchar(50),
-    email varchar(50),
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    izlozba int not null,
+    oib char(11) not null,
+    email varchar(100),
     iban varchar(50)
 );
 
 create table sponzor(
-    naziv varchar(50),
-    izlozba varchar(50),
-    iznos varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    izlozba int not null,
+    iznos decimal(18,2)
 );
