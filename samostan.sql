2640ddb9beae30d4c32cd94be82a28030a438c00
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-11 18:05:41 +0200

    Update samostan.sql
    odabrani tipovi podataka i dodane veze

--- samostan.sql
+++ samostan.sql
@@ -1,28 +1,49 @@
 # U samostanu se nalaze svećenici. Svaki svećenik je zadužen za više poslova. Jedan posao u isto vrijeme može obavljati više svećenika. 
 # Svaki svećenik ima samo jednog nadređenog svećenika
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\vjezbe\samostan.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\vjezbe\samostan.sql
 
 drop database if exists samostan;
 create database samostan;
 use samostan;
 
 create table poslovi(
-    naziv varchar(50),
-    mjesto varchar(50),
-    pocetak varchar(50),
-    zavrsetak varchar(50)
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    mjesto varchar(50) not null,
+    pocetak datetime not null,
+    zavrsetak datetime
 );
 
 create table zaduzenje(
-    poslovi varchar(50),
-    svecenik varchar(50)
+    sifra int not null primary key auto_increment,
+    poslovi int not null,
+    svecenik int not null
 );
 
 create table svecenik(
-    ime varchar(50),
-    prezime varchar(50),
-    datumrodjenja varchar(50),
-    oib varchar(50),
-    nadredjenisvecenik varchar(50),
-    email varchar(50)
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    nadredjenisvecenik int not null
 );
+
+create table osoba(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    datumrodjenja datetime,
+    oib char(11) not null,
+    email varchar(100)
+);
+
+create table nadredjenisvecenik(
+    sifra int not null primary key auto_increment,
+    osoba int not null
+);
+
+alter table zaduzenje add foreign key (poslovi) references poslovi(sifra);
+alter table zaduzenje add foreign key (svecenik) references svecenik(sifra);
+
+alter table nadredjenisvecenik add foreign key (osoba) references osoba(sifra);
+alter table svecenik add foreign key (osoba) references osoba(sifra);
+
+alter table svecenik add foreign key (nadredjenisvecenik) references nadredjenisvecenik(sifra);
