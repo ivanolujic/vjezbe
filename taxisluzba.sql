ad5ba7f696fdb333ca0eff4e7fb2b1e4e758dd79
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-14 21:50:40 +0200

    Update taxisluzba.sql
    postavljanje tipova podataka

--- taxisluzba.sql
+++ taxisluzba.sql
@@ -1,41 +1,43 @@
 # Taksi tvrtka ima više vozila. Jedno vozilo vozi jedan vozač. U jednoj vožnji vozač može prevesti više putnika. 
 # Jedan putnik se naravno može tijekom vremena imati više vožnji.
-# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Korisnik\Documents\GitHub\vjezbe\taxisluzba.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\vjezbe\taxisluzba.sql
 
 drop database if exists taxisluzba;
 create database taxisluzba;
 use taxisluzba;
 
 create table vozilo(
-    marka varchar(50),
-    tip varchar(50),
-    model varchar(50),
-    registracija varchar(50),
-    godiste varchar(50),
-    vozac varchar(50)
+    sifra int not null primary key auto_increment,
+    marka varchar(50) not null,
+    tip varchar(50) not null,
+    model varchar(50) not null,
+    registracija varchar(50) not null,
+    godiste datetime,
+    vozac varchar(50) not null
 );
 
 create table vozac(
-    osoba varchar(50),
-    datumrodjenja varchar(50),
-    iban varchar(50),
-    vozilo varchar(50)
-);
-
-create table osoba(
-    ime varchar(50),
-    prezime varchar(50),
-    oib varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11) not null,
+    datumrodjenja datetime not null,
+    iban varchar(50) not null,
+    vozilo int not null
 );
 
 create table putnik(
-    osoba varchar(50),
-    email varchar(50),
-    adresa varchar(50)
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11),
+    email varchar(100),
+    adresa varchar(50) not null
 );
 
 create table voznja(
-    vozilo varchar(50),
-    vozac varchar(50),
-    putnik varchar(50)
+    sifra int not null primary key auto_increment,
+    vozilo int,
+    vozac int,
+    putnik int
 );
