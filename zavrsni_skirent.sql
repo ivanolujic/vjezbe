0894eef9c05e901d47922e7c7769fe7e23d75db6
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-20 11:26:05 +0200

    Update zavrsni_skirent.sql

--- zavrsni_skirent.sql
+++ zavrsni_skirent.sql
@@ -5,32 +5,14 @@ drop database if exists zavrsni_skirent;
 create database zavrsni_skirent;
 use zavrsni_skirent;
 
-create table skije(
+create table oprema(
     sifra int not null primary key auto_increment,
+    vrsta varchar(50) not null,
     marka varchar(50),
-    model varchar(50),
+    velicina varchar(50) not null,
     duzina decimal(3,0),
-    radijus decimal(2,0),
-    serviser int,
-    kupac int
-);
-
-create table pancerice(
-    sifra int not null primary key auto_increment,
-    marka varchar(50),
-    model varchar(50),
-    velicina decimal(2,0),
-    boja varchar(50),
-    kupac int
-);
-
-create table stapovi(
-    sifra int not null primary key auto_increment,
-    marka varchar(50),
-    model varchar(50),
-    duzina decimal(3,0),
-    disciplina varchar(50),
-    kupac int
+    radijus decimal(2,0)
+    
 );
 
 create table serviser(
@@ -52,24 +34,14 @@ create table kupac(
 
 create table usluga(
     sifra int not null primary key auto_increment,
+    oprema int,
     kupac int,
     serviser int,
-    skije int,
-    pancerice int,
-    stapovi int,
-    datum datetime,
+    datumpocetka datetime not null,
+    datumkraja datetime,
     cijena decimal(16,2)
 );
 
 alter table usluga add foreign key (kupac) references kupac(sifra);
 alter table usluga add foreign key (serviser) references serviser(sifra);
-alter table usluga add foreign key (skije) references skije(sifra);
-alter table usluga add foreign key (pancerice) references pancerice(sifra);
-alter table usluga add foreign key (stapovi) references stapovi(sifra);
-
-alter table skije add foreign key (serviser) references serviser(sifra);
-
-alter table  skije add foreign key (kupac) references kupac(sifra);
-alter table  pancerice add foreign key (kupac) references kupac(sifra);
-alter table  stapovi add foreign key (kupac) references kupac(sifra);
-
+alter table usluga add foreign key (oprema) references oprema(sifra);
