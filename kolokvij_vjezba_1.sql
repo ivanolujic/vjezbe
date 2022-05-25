f9164c32c4d16213c8a6edf3140dedab1ae3f52c
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-25 22:28:53 +0200

    Update kolokvij_vjezba_1.sql

--- kolokvij_vjezba_1.sql
+++ kolokvij_vjezba_1.sql
@@ -31,3 +31,51 @@ create table zena(
     haljina varchar(44),
     sestra int not null
 );
+
+create table cura(
+    sifra int not null primary key auto_increment,
+    novcica decimal(16,5) not null,
+    gustoca decimal(18,6) not null,
+    lipa decimal(13,10),
+    ogrlica int not null,
+    bojakose varchar(38),
+    suknja varchar(36),
+    punac int
+);
+
+create table sestra_svekar(
+    sifra int not null primary key auto_increment,
+    sestra int,
+    svekar int
+);
+
+create table svekar(
+    sifra int not null primary key auto_increment,
+    bojaociju varchar(40) not null,
+    prstena int,
+    dukserica varchar(41),
+    lipa decimal(13,8),
+    eura decimal(12,7),
+    majica varchar(35)
+);
+
+create table muskarac(
+    sifra int not null primary key auto_increment,
+    bojaociju varchar(50) not null,
+    hlace varchar(30),
+    modelnaocala varchar(43),
+    maraka decimal(14,5) not null,
+    zena int not null
+);
+
+create table mladic(
+    sifra int not null primary key auto_increment,
+    suknja varchar(50) not null,
+    kuna decimal(16,8) not null,
+    drugiputa datetime,
+    asocijalno bit,
+    ekstrovertno bit not null,
+    dukserica varchar(48) not null,
+    muskarac int
+);
+
