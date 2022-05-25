ac3bcf49180d22ac713cab55815bb0b270445622
IvanOlujic <ivan.olujic.os@gmail.com>
2022-05-25 22:16:08 +0200

    Create kolokvij_vjezba_1.sql

---
+++ kolokvij_vjezba_1.sql
@@ -0,0 +1,33 @@
+# Kolokvij
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\vjezbe\kolokvij_vjezba_1.sql
+
+drop database if exists kolokvij_vjezba_1;
+create database kolokvij_vjezba_1;
+use kolokvij_vjezba_1;
+
+create table sestra(
+    sifra int not null primary key auto_increment,
+    introvertno bit,
+    haljina varchar(31) not null,
+    maraka decimal(16,6),
+    hlace varchar(46) not null,
+    narukvica int not null
+);
+
+create table punac(
+    sifra int not null primary key auto_increment,
+    ogrlica int,
+    gustoca decimal(14,9),
+    hlace varchar(41) not null
+);
+
+create table zena(
+    sifra int not null primary key auto_increment,
+    treciputa datetime,
+    hlace varchar(46),
+    kratkamajica varchar(31) not null,
+    jmbg char(11) not null,
+    bojaociju varchar(39) not null,
+    haljina varchar(44),
+    sestra int not null
+);
