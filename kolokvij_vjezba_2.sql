6f5e42d38437cd49cff9657eb7d217d54efdc672
IvanOlujic <ivan.olujic.os@gmail.com>
2022-06-02 20:14:16 +0200

    Create kolokvij_vjezba_2.sql

---
+++ kolokvij_vjezba_2.sql
@@ -0,0 +1,77 @@
+# Kolokvij
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Korisnik\Documents\GitHub\vjezbe\kolokvij_vjezba_2.sql
+# C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\Ivan\Documents\GitHub\vjezbe\kolokvij_vjezba_2.sql
+
+drop database if exists kolokvij_vjezba_2;
+create database kolokvij_vjezba_2;
+use kolokvij_vjezba_2;
+
+create table decko(
+    sifra int not null primary key auto_increment,
+    indiferentno bit,
+    vesta varchar(34),
+    asocijalno bit not null
+);
+
+create table prijatelj(
+    sifra int not null primary key auto_increment,
+    modelnaocala varchar(37),
+    treciputa datetime not null,
+    ekstroventno bit not null,
+    prviputa datetime,
+    svekar int not null
+);
+
+create table cura(
+    sifra int not null primary key auto_increment,
+    haljina varchar(33) not null,
+    drugiputa datetime not null,
+    suknja varchar(38),
+    narukvica int,
+    introventno bit,
+    majica varchar(40) not null,
+    decko int
+);
+
+create table decko_zarucnica(
+    sifra int not null primary key auto_increment,
+    decko int not null,
+    zarucnica int not null
+);
+
+create table svekar(
+    sifra int not null primary key auto_increment,
+    stilfrizura varchar(48),
+    ogrlica int not null,
+    asocijalno bit not null
+);
+
+create table zarucnica(
+    sifra int not null primary key auto_increment,
+    narukvica int,
+    bojakose varchar(37) not null,
+    novcica decimal(15,8),
+    lipa decimal(15,8) not null,
+    indiferentno bit not null
+);
+
+create table brat(
+    sifra int not null primary key auto_increment,
+    suknja varchar(47),
+    ogrlica int not null,
+    asocijalno bit not null,
+    neprijatelj int not null
+);
+
+create table neprijatelj(
+    sifra int not null primary key auto_increment,
+    majica varchar(32),
+    haljina varchar(43) not null,
+    lipa decimal(16,8),
+    modelnaocala varchar(49) not null,
+    kuna decimal(12,6) not null,
+    jmbag char(11),
+    cura bit
+);
+
+
